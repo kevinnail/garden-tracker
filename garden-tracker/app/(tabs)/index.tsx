02d92370077d4c0bfeeb631fbb4727e37af2d74f
kevinnail <fumalicious@gmail.com>
2026-04-08 16:22:24 -0700

    Update PlannerScreen tab entry point

--- garden-tracker/app/(tabs)/index.tsx
+++ garden-tracker/app/(tabs)/index.tsx
@@ -1,12 +1,14 @@
 import { View, StyleSheet } from 'react-native';
 
 import { usePlannerData } from '@/src/hooks/usePlannerData';
+import PlannerToolbar from '@/src/components/toolbar/PlannerToolbar';
 import PlannerGrid from '@/src/components/planner/PlannerGrid';
 
 export default function PlannerScreen() {
   usePlannerData();
   return (
     <View style={styles.container}>
+      <PlannerToolbar />
       <PlannerGrid />
     </View>
   );
