fe017a15bf139e80fe9ee541578521b8171d7366
kevinnail <fumalicious@gmail.com>
2026-04-09 10:06:25 -0700

    Minor whitespace cleanup in PlannerScreen

--- garden-tracker/app/(tabs)/index.tsx
+++ garden-tracker/app/(tabs)/index.tsx
@@ -6,6 +6,7 @@ import PlannerGrid from '@/src/components/planner/PlannerGrid';
 
 export default function PlannerScreen() {
   usePlannerData();
+
   return (
     <View style={styles.container}>
       <PlannerToolbar />
