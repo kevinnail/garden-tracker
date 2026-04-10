8b044a96be7ec5047a223beb504fcfd8dca8f97c
kevinnail <fumalicious@gmail.com>
2026-04-09 23:52:04 -0700

    Update edit-crop modal

--- garden-tracker/app/(modals)/edit-crop.tsx
+++ garden-tracker/app/(modals)/edit-crop.tsx
@@ -1,5 +1,12 @@
+import Toast from 'react-native-toast-message';
+
 import EditCropModal from '@/src/components/forms/EditCropModal';
 
 export default function EditCropRoute() {
-  return <EditCropModal />;
+  return (
+    <>
+      <EditCropModal />
+      <Toast />
+    </>
+  );
 }
