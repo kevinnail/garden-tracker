84ce2a1dc46157d33d046416c138794a9057e7d1
kevinnail <fumalicious@gmail.com>
2026-04-09 23:51:51 -0700

    Update add-garden modal

--- garden-tracker/app/(modals)/add-garden.tsx
+++ garden-tracker/app/(modals)/add-garden.tsx
@@ -1,5 +1,12 @@
+import Toast from 'react-native-toast-message';
+
 import AddGardenForm from '@/src/components/forms/AddGardenForm';
 
 export default function AddGardenModal() {
-  return <AddGardenForm />;
+  return (
+    <>
+      <AddGardenForm />
+      <Toast />
+    </>
+  );
 }
