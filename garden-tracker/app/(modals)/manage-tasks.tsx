e497fcb49dcf0c8938733c1a0990674b408f4ae4
kevinnail <fumalicious@gmail.com>
2026-04-09 23:52:09 -0700

    Update manage-tasks modal

--- garden-tracker/app/(modals)/manage-tasks.tsx
+++ garden-tracker/app/(modals)/manage-tasks.tsx
@@ -1,5 +1,12 @@
+import Toast from 'react-native-toast-message';
+
 import TaskAssessForm from '@/src/components/forms/TaskAssessForm';
 
 export default function ManageTasksModal() {
-  return <TaskAssessForm />;
+  return (
+    <>
+      <TaskAssessForm />
+      <Toast />
+    </>
+  );
 }
