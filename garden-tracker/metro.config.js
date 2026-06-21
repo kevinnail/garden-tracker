856f94095e5b3f94b3b6dfc84d0c67789943a5a4
kevinnail <fumalicious@gmail.com>
2026-06-21 16:44:59 -0700

    Update metro config for better-auth resolution

--- garden-tracker/metro.config.js
+++ garden-tracker/metro.config.js
@@ -5,6 +5,11 @@ const config = getDefaultConfig(__dirname);
 
 config.resolver.assetExts.push('wasm');
 
+// better-auth (and @better-auth/core) ship their subpaths via the package
+// "exports" map (e.g. @better-auth/core/utils/string). Metro doesn't resolve
+// those subpaths unless package-exports resolution is enabled.
+config.resolver.unstable_enablePackageExports = true;
+
 config.server.enhanceMiddleware = (middleware) => {
   return (req, res, next) => {
     res.setHeader('Cross-Origin-Embedder-Policy', 'credentialless');
